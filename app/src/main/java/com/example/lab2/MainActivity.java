b0f34b68e593102c3af8702b5b79153cf15088e3
mashaelalbu <mashaelalbu@gmail.com>
2023-09-04 11:26:14 +0300

    lab

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import  android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button send_button;
+    EditText send_text;
+    TextView received_value;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        send_button= findViewById(R.id.send_button_id);
+        send_text = findViewById(R.id.send_text_id);
+        received_value =findViewById(R.id.received_value_id);
+
+
+        send_button.setOnClickListener(v -> {
+
+            String str = send_text.getText().toString();
+
+            received_value.setText("hello "+str);
+        });
+
+
     }
+
 }
